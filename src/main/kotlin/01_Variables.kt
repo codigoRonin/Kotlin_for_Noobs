a91fabdbb686d2dd1d7616e4d42cac2203510175
Michel <codigoronin@gmail.com>
2023-09-20 14:20:30 +0200

    Variables

---
+++ src/main/kotlin/01_Variables.kt
@@ -0,0 +1,47 @@
+fun main() {
+
+    //TIPOS DE DATOS
+    /*
+        •	Tipo texto (String), representados con comillas dobles
+        •	Tipo carácter (Char), representados con comillas simples
+        •	Tipo booleano (Boolean), con los valores true y false
+        •	Tipo real (Float de 4 bytes o Double de 8 bytes, dependiendo de la precisión).
+            En este caso, para constantes literales de tipo float se le debe indicar una f al final,
+            como en Java: var numero = 1.345f
+        •	Tipo entero (Int). Ocupa 4 bytes.
+            También tenemos los tipos Short y Byte para enteros de menor tamaño
+            (2 bytes y 1 byte, respectivamente)
+      */
+
+// DECLARACION DE VARIABLES
+
+    //INFERENCIA DE TIPO
+
+        var nombre = "Michel"
+        var numero = 123
+
+        println(numero) //123
+        println(nombre) //Michel
+
+    var numero2 : Int = 32
+    var nota : Double = 8.75
+    var letra : Char = 's'
+    var existe : Boolean = true
+
+//CONSTANTES
+
+    val fichero = "datos.dat"
+  //  const val FICHERO = "datos.dat"
+
+    val xPos: Int = 1 // Asignación junto a declaración
+    val yPos: Int   // Declaración
+    yPos = 5    // Asignación
+
+    println(xPos)
+    println(yPos)
+
+//    yPos= 6 // Kotlin: Val cannot be reassigned
+//    println(yPos)
+
+
+}
