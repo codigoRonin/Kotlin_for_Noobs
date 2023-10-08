49e5bb61633e35245c5a72780b3434ce39e10a2a
Michel <codigoronin@gmail.com>
2023-10-08 13:48:07 +0200

    Listas + Ejercicio 5

--- src/main/kotlin/Ejercicio_05.kt
+++ src/main/kotlin/Ejercicio_05.kt
@@ -11,6 +11,14 @@
  *
  */
 
-fun main(){
+fun main() {
+    println(isAnagram("amor", "roma"))
+}
 
+private fun isAnagram(wordOne: String, wordTwo: String): Boolean {
+
+    if (wordOne.lowercase() == wordTwo.lowercase()) {
+        return false
+    }
+    return wordOne.lowercase().toCharArray().sortedArray().contentEquals(wordTwo.lowercase().toCharArray().sortedArray())
 }
---
+++ src/main/kotlin/Ejercicio_06.kt
@@ -0,0 +1,20 @@
+/*
+ * Enunciado:
+ *
+ *  Crea una lista llamada edades que contenga al menos 7 edades de personas (números enteros).
+ *
+ * Escribe una función llamada promedioEdades que tome la lista de edades como parámetro y
+ * devuelva el promedio de edades como un número decimal.
+ *
+ * Escribe una función llamada mayorEdad que tome la lista de edades como parámetro y devuelva la edad más alta.
+ *
+ * Escribe una función llamada menorEdad que tome la lista de edades como parámetro y devuelva la edad más baja.
+ *
+ * Llama a las tres funciones (promedioEdades, mayorEdad y menorEdad) con la lista edades
+ * y muestra los resultados en la consola.
+ */
+
+fun main(){
+
+
+}
